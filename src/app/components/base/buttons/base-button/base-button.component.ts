3550461f13e68c434d923c77d4446710a1df7fba
Maxime Legare <mleg1234@protonmail.com>
2022-07-12 09:27:31 -0400

    add button with border

--- src/app/components/base/buttons/base-button/base-button.component.ts
+++ src/app/components/base/buttons/base-button/base-button.component.ts
@@ -7,34 +7,44 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BaseButtonComponent implements OnInit {
   @Input() text: string;
-  @Input() variant: string 
+  @Input() variant: string;
   @Input() icon: string;
   @Input() type?: string;
-  @Input() underlineOnHover?:boolean = false;
-  @Input() color?:string = 'accent';
-  @Input() size?:string = 'small';
-
+  @Input() underlineOnHover?: boolean = false;
+  @Input() color?: string = 'accent';
+  @Input() size?: string = 'small';
 
   constructor() {}
 
-  ngOnInit(): void {
-
-    
-  }
+  ngOnInit(): void {}
 
   getStyles() {
     switch (this.variant) {
       case 'text-only': {
-        return `${this.size === "big"? "py-3 px-5 text-xl": "py-1 px-3 text-md" } text-black  font-semibold hover:opacity-100 ${this.underlineOnHover? "hover:underline":""}`;
+        return `${
+          this.size === 'big' ? 'py-3 px-5 text-xl' : 'py-1 px-3 text-md'
+        } text-black  font-semibold hover:opacity-100 ${
+          this.underlineOnHover ? 'hover:underline' : ''
+        }`;
       }
       case 'text-button': {
-        return `${this.size === "big"? "py-3 px-5 text-xl": "py-1 px-3 text-md" } text-black bg-${this.color} rounded-md shadow-sm font-semibold ${this.underlineOnHover? "hover:underline":""} hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
+        return `${
+          this.size === 'big' ? 'py-3 px-5 text-xl' : 'py-1 px-3 text-md'
+        } text-black bg-${this.color}  rounded-md shadow-sm font-semibold ${
+          this.underlineOnHover ? 'hover:underline' : ''
+        } hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
+      }
+      case 'text-button-border': {
+        return `${
+          this.size === 'big' ? 'py-3 px-5 text-xl' : 'py-1 px-3 text-md'
+        } text-black border-solid border-2 border-${
+          this.color
+        } rounded-md shadow-sm font-semibold ${
+          this.underlineOnHover ? 'hover:underline' : ''
+        } hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
       }
       case 'round': {
         return `p-4 text-black bg-${this.color} rounded-full shadow-sm hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
-      }
-      case 'outlined':{
-        return `${this.size === "big"? "py-3 px-5 text-xl": "py-1 px-3 text-md" } text-black outline-${this.color} rounded-md shadow-sm font-semibold ${this.underlineOnHover? "hover:underline":""} hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
       }
       default: {
         return ``;
