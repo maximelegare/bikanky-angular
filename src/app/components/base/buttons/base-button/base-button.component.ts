1381ae2e511378a9370e103ff4329be599224205
Maxime Legare <mleg1234@protonmail.com>
2022-07-11 23:44:32 -0400

    add size and color props to base-button, work on hero-section layout

--- src/app/components/base/buttons/base-button/base-button.component.ts
+++ src/app/components/base/buttons/base-button/base-button.component.ts
@@ -11,7 +11,8 @@ export class BaseButtonComponent implements OnInit {
   @Input() icon: string;
   @Input() type?: string;
   @Input() underlineOnHover?:boolean = false;
-
+  @Input() color?:string = 'accent';
+  @Input() size?:string = 'small';
 
 
   constructor() {}
@@ -24,13 +25,16 @@ export class BaseButtonComponent implements OnInit {
   getStyles() {
     switch (this.variant) {
       case 'text-only': {
-        return `py-1 px-3 text-black  font-semibold hover:opacity-100 ${this.underlineOnHover? "hover:underline":""}`;
+        return `${this.size === "big"? "py-3 px-5 text-xl": "py-1 px-3 text-md" } text-black  font-semibold hover:opacity-100 ${this.underlineOnHover? "hover:underline":""}`;
       }
       case 'text-button': {
-        return `py-1 px-3 text-black bg-accent rounded-md shadow-sm font-semibold ${this.underlineOnHover? "hover:underline":""} hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
+        return `${this.size === "big"? "py-3 px-5 text-xl": "py-1 px-3 text-md" } text-black bg-${this.color} rounded-md shadow-sm font-semibold ${this.underlineOnHover? "hover:underline":""} hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
       }
       case 'round': {
-        return `p-4 text-black bg-accent rounded-full shadow-sm hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
+        return `p-4 text-black bg-${this.color} rounded-full shadow-sm hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
+      }
+      case 'outlined':{
+        return `${this.size === "big"? "py-3 px-5 text-xl": "py-1 px-3 text-md" } text-black outline-${this.color} rounded-md shadow-sm font-semibold ${this.underlineOnHover? "hover:underline":""} hover:scale-105 transition-scale duration-100 hover:opacity-100  `;
       }
       default: {
         return ``;
