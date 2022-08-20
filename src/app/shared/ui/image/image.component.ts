413130c16fd0ea07a06e207f8e31ad9e32cdc6de
Maxime Legare <mleg1234@protonmail.com>
2022-08-20 10:56:56 -0400

    add image size prop

--- src/app/shared/ui/image/image.component.ts
+++ src/app/shared/ui/image/image.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 
-
-
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
-  styleUrls: ['./image.component.css']
+  styleUrls: ['./image.component.css'],
 })
 export class ImageComponent implements OnInit {
+  constructor() {}
+  @Input() src: string;
+  @Input() width?: string  = 'w-40'
+  @Input() height?: string = 'h-60'
 
-  constructor() { }
-  @Input() src:string;
-  
-  ngOnInit(): void {
-    console.log(this.src)
+  ngOnInit(): void {}
+  getStyles() {
+    return `${this.width} ${this.height} overflow-hidden rounded-lg mx-auto bg-slate-200`;
   }
-
 }
